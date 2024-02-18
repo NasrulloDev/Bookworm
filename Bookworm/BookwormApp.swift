39ade181f29e33c8943b7118289271edeab9a1da
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-02-18 10:34:22 +0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Насрулло Исмоилжонов on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
