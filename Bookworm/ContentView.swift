39200ac027e74a05b484cf00c03521e16795bab2
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-02-18 11:57:02 +0500

    SwiftData
    Introduction to SwiftData and SwiftUI

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -4,17 +4,31 @@
 //
 //  Created by Насрулло Исмоилжонов on 18/02/24.
 //
-
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("notes") private var notes = ""
+    @Environment(\.modelContext) var modelContext
+    @Query var students: [Student]
     
     var body: some View {
         NavigationStack{
-            TextEditor(text: $notes)
-                .navigationTitle("Notes")
-                .padding()
+            List(students){ student in
+                Text(student.name)
+            }
+            .navigationTitle("Classroom")
+            .toolbar{
+                Button("Add"){
+                    let firstNames = ["Nasrullo", "Jamshid", "Zaman", "Ulugbek", "Fazliddin"]
+                    let lastNames = ["Duysenbaev", "Baxromov", "Ismoiljonov", "Nizomhujaev", "Mavlonov"]
+                    
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+                    
+                    let student = Student(id: UUID(), name: "\(chosenLastName) \(chosenFirstName)")
+                    modelContext.insert(student)
+                }
+            }
         }
     }
 }
