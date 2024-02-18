f9c8bc14622274e59a00b5b0e872da317a194a90
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-02-18 11:59:49 +0500

    Deleted all
    Ready to start the real project

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -4,32 +4,18 @@
 //
 //  Created by Насрулло Исмоилжонов on 18/02/24.
 //
-import SwiftData
+
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.modelContext) var modelContext
-    @Query var students: [Student]
-    
     var body: some View {
-        NavigationStack{
-            List(students){ student in
-                Text(student.name)
-            }
-            .navigationTitle("Classroom")
-            .toolbar{
-                Button("Add"){
-                    let firstNames = ["Nasrullo", "Jamshid", "Zaman", "Ulugbek", "Fazliddin"]
-                    let lastNames = ["Duysenbaev", "Baxromov", "Ismoiljonov", "Nizomhujaev", "Mavlonov"]
-                    
-                    let chosenFirstName = firstNames.randomElement()!
-                    let chosenLastName = lastNames.randomElement()!
-                    
-                    let student = Student(id: UUID(), name: "\(chosenLastName) \(chosenFirstName)")
-                    modelContext.insert(student)
-                }
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
